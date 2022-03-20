e9d563463c447303a7add388b10a78a5e2108f12
jaimedaniels94 <jaimedaniels94@gmail.com>
2022-03-20 12:32:10 -0500

    added subtitle to tests

--- tests/UnitTests/ApplicationCore/Entities/CatalogItemTests/UpdateDetails.cs
+++ tests/UnitTests/ApplicationCore/Entities/CatalogItemTests/UpdateDetails.cs
@@ -11,38 +11,39 @@ public class UpdateDetails
     private int _validBrandId = 2;
     private string _validDescription = "test description";
     private string _validName = "test name";
+    private string _validSubtitle = "test subtitle";
     private decimal _validPrice = 1.23m;
     private string _validUri = "/123";
 
     public UpdateDetails()
     {
-        _testItem = new CatalogItem(_validTypeId, _validBrandId, _validDescription, _validName, _validPrice, _validUri);
+        _testItem = new CatalogItem(_validTypeId, _validBrandId, _validDescription, _validName, _validSubtitle, _validPrice, _validUri);
     }
 
     [Fact]
     public void ThrowsArgumentExceptionGivenEmptyName()
     {
         string newValue = "";
-        Assert.Throws<ArgumentException>(() => _testItem.UpdateDetails(newValue, _validDescription, _validPrice));
+        Assert.Throws<ArgumentException>(() => _testItem.UpdateDetails(newValue, _validSubtitle, _validDescription, _validPrice));
     }
 
     [Fact]
     public void ThrowsArgumentExceptionGivenEmptyDescription()
     {
         string newValue = "";
-        Assert.Throws<ArgumentException>(() => _testItem.UpdateDetails(_validName, newValue, _validPrice));
+        Assert.Throws<ArgumentException>(() => _testItem.UpdateDetails(_validName, _validSubtitle, newValue, _validPrice));
     }
 
     [Fact]
     public void ThrowsArgumentNullExceptionGivenNullName()
     {
-        Assert.Throws<ArgumentNullException>(() => _testItem.UpdateDetails(null, _validDescription, _validPrice));
+        Assert.Throws<ArgumentNullException>(() => _testItem.UpdateDetails(null, _validSubtitle, _validDescription, _validPrice));
     }
 
     [Fact]
     public void ThrowsArgumentNullExceptionGivenNullDescription()
     {
-        Assert.Throws<ArgumentNullException>(() => _testItem.UpdateDetails(_validName, null, _validPrice));
+        Assert.Throws<ArgumentNullException>(() => _testItem.UpdateDetails(_validName, _validSubtitle, null, _validPrice));
     }
 
     [Theory]
@@ -50,6 +51,6 @@ public class UpdateDetails
     [InlineData(-1.23)]
     public void ThrowsArgumentExceptionGivenNonPositivePrice(decimal newPrice)
     {
-        Assert.Throws<ArgumentException>(() => _testItem.UpdateDetails(_validName, _validDescription, newPrice));
+        Assert.Throws<ArgumentException>(() => _testItem.UpdateDetails(_validName, _validSubtitle, _validDescription, newPrice));
     }
 }
